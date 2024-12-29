9d8119c3a0cabf1a3b2e76015537dfa05559f318
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-12-29 18:02:40 +0330

    add nav-link component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,11 +3,12 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-backgroung";
+import NavLnik from "./nav-link";
 
 export default function MainHeader() {
   return (
     <>
-      <MainHeaderBackground/>
+      <MainHeaderBackground />
       <header className={classes.header}>
         <Link href="/" className={classes.logo}>
           <Image src={logoImg} alt="A plate food on it" priority />
@@ -16,10 +17,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Meals</Link>
+              <NavLnik href="/meals">Browse Meals</NavLnik>
             </li>
             <li>
-              <Link href="/community">Community</Link>
+              <NavLnik href="/community">community</NavLnik>
             </li>
           </ul>
         </nav>
