3950e9ec08534cbd311bb9ca67e6ad09f662d0f6
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-12-31 18:54:11 +0330

    add slug and xxs

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,6 @@
-
-import ImagePricker from '@/components/meals/image-picker';
-import classes from './page.module.css';
+import ImagePricker from "@/components/meals/image-picker";
+import classes from "./page.module.css";
+import { shareMeal } from "@/lib/action";
 
 export default function ShareMealPage() {
   return (
@@ -12,7 +12,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,7 +40,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePricker />
+          <ImagePricker label="your Image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
