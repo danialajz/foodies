f3dc4cf8496baa45a4a38a499f022215d08baa61
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-12-31 18:11:37 +0330

    handel privew photo

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,15 +1,35 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 export default function ImagePricker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInputRef = useRef();
   function hanelClickPicker() {
     imageInputRef.current.click();
+  }
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   }
   return (
     <div className={classes.picker}>
       <label htmlFor="image">{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="the image selected by a user" fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -17,6 +37,7 @@ export default function ImagePricker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInputRef}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
